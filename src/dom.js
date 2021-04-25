f41e516621fe74d5cde070642bad007236eab88a
cesar iduarte <ciduarte@gmail.com>
2021-04-24 19:25:49 -0500

    Init repo

---
+++ src/dom.js
@@ -0,0 +1,7 @@
+function $(ele) {
+    return document.querySelector(ele);
+}
+
+function $$(ele) {
+    return document.querySelectorAll(ele);
+}
---
+++ src/index.js
@@ -0,0 +1,1 @@
+$('body').innerText = 'Webpack demo';
