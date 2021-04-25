90fb026fa0a82584e61bcec3d70e6c92e119aac3
cesar iduarte <ciduarte@gmail.com>
2021-04-24 21:09:16 -0500

    Fix webpack output path error

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: './dist'
+        path: path.resolve(__dirname, './dist')
     },
     mode: 'none'
 }
