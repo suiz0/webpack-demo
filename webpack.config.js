7a28c1fb2de2244b87045dbb8a7bd39aeb9b5cd2
cesar iduarte <ciduarte@gmail.com>
2021-05-23 16:03:27 -0500

    Add some useful plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+// Cargamos el plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Cargamos el plugin
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require('path');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './dist')
     },
-    mode: 'none'
+    mode: 'none',
+    plugins: [ // Lo registramos
+        new CleanWebpackPlugin({
+            verbose: true
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Webpack Demo'
+        })
+    ]
 }
